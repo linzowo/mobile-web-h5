189a7f7bd6b0c9a0080323cd6724cf4162b17753
linzowo <linzowo@outlook.com>
2019-11-10 18:45:20 +0800

    万圣节-01：解决移动端设备尺寸适配问题

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,9 @@ import show from "./index.js";
 // 引入样式文件
 import '../css/common.scss';
 
+// 解决移动端样式尺寸适配问题。动态计算rem的基准值
+document.documentElement.style.fontSize = document.documentElement.clientWidth/30 + "px";
+
 // test
 let container = document.createElement("div");
 container.innerHTML = "hello webpack";
