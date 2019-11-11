57641f65aabefd756d30342db77259399e57c94d
linzowo <linzowo@outlook.com>
2019-11-11 11:09:32 +0800

    万圣节-02：引入animate动画css库

--- 01-halloween/src/js/main.js
+++ 01-halloween/src/js/main.js
@@ -5,15 +5,16 @@ import show from "./index.js";
 
 // 引入样式文件
 import '../css/common.scss';
+import "../css/1.scss";
+
+// 引入动画样式库
+import "../css/animate.css"
 
 // 解决移动端样式尺寸适配问题。动态计算rem的基准值
 document.documentElement.style.fontSize = document.documentElement.clientWidth/30 + "px";
 
-// test
-let container = document.createElement("div");
-container.innerHTML = "hello webpack";
-document.querySelector("body").appendChild(container);
 
+// 热模块更新处理区域
 if (module.hot) {
   module.hot.accept("./index.js", function() {
     console.log("Accepting the updated show module!");
